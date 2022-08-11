3c9d2c1a69521645030a38173aa852355e3250b2
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-08-11 20:29:54 +0530

    Update LocalThemedBox.js

--- src/components/LocalThemedBox.js
+++ src/components/LocalThemedBox.js
@@ -11,7 +11,7 @@ const LocalThemedBox = () => {
       },[themeToggle.theme])
 return(
     <div className={`bg-${localTheme}`} style={{width:'200px',height:'200px',border:'2px solid green'}} id="local-themed-box">
-        <p className={`txt-${localTheme}`} id="local-themed-text-container">Hi this is local theme</p>
+        <p className={`txt-${localTheme}`} id="local-themed-text-container">Hiiii</p>
         <button className={`btn btn-${localTheme} txt-${localTheme}`} onClick={() => setLocalTheme(themeName)}
         id="global-theme-toggler">Switch to { themeName } theme</button>
     </div>
