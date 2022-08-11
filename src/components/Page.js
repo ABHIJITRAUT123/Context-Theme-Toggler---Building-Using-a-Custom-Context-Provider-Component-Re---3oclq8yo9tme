e092a0e55c1a6e04b162cdfc78732dc823c70bcb
ABHIJITRAUT123 <106418992+ABHIJITRAUT123@users.noreply.github.com>
2022-08-11 19:47:54 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { LocalThemedBox } from './LocalThemedBox';
 import { ThemeContext } from './ThemeProvider';
 import '../styles/App.css';
